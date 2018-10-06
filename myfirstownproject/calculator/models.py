2b67d8d90f974fd62b888e03d3fabd43ad7c6bfa
Mateusz B <mateusz.emmanuel47@gmail.com>
2018-10-06 17:33:59 +0200

    new file:   myfirstownproject/calculator/__pycache__/admin.cpython-36.pyc 	new file:   myfirstownproject/calculator/__pycache__/apps.cpython-36.pyc 	new file:   myfirstownproject/calculator/__pycache__/models.cpython-36.pyc 	modified:   myfirstownproject/calculator/__pycache__/views.cpython-36.pyc 	new file:   myfirstownproject/calculator/migrations/__pycache__/__init__.cpython-36.pyc 	modified:   myfirstownproject/calculator/models.py 	new file:   myfirstownproject/calculator/templates/calculator/calculator.html 	modified:   myfirstownproject/calculator/views.py 	modified:   myfirstownproject/myfirstownproject/__pycache__/settings.cpython-36.pyc 	modified:   myfirstownproject/myfirstownproject/settings.py

--- myfirstownproject/calculator/models.py
+++ myfirstownproject/calculator/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class CalculatorModel(models.Model):
+	def calc_add(self,a,b):
+		 return a+b
+	def calc_sub(self,a,b):
+		return a-b
+	def calc_sq(self,a):
+		return a*a
+	def calc_div(self,a,b):
+		return a/b
+	def calc_mul(self,a,b):
+		return a*b
